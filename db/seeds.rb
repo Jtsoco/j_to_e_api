cb8725716bfee5b760831ebb730f5cd1727fab50
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-28 21:44:36 +0900

    lots of starter things, just figuring out the dictionary parser

---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+i = {}
+Eiwa.parse_file("app/JMdict_e.xml", type: :jmdict_e) do |entry|
+  i = entry
+end
+# gives us the meaning hash
+entry.meanings.first
+# gives array of all definitions
+entry.meanings.first.definitions
+# gives first of the definitions, might not always be array
+entry.meanings.first.definitions.first
+# gives english characters
+entry.meanings.first.definitions.first.characters
+# gives language (english only in this dicitonary?
+entry.meanings.first.definitions.first.language
+# gives a lot of other info
+entry.meanings.first.parent
+# gives japanese reading, furigana
+# entry.meanings.first.parent.readings.first.text
+entry.readings.first.text
+# gives the spelling with kanji might not always have array
+entry.spellings.first.text
+# gets misc tags, like idiomatic expression
+entry.meanings.first.misc_tags.first.text
+# returns an array of things, like if it's a phrase or a noun depending on the .text of the element
+meanings.first.parts_of_speech #.first.text, [1].text or whichever
+# gives the japanese
+entry.text
