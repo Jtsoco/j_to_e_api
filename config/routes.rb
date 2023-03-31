3597acf1a8f65a94147bfab6a8365c32e5a698fb
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-31 16:35:24 +0900

    added api keys controller, and set three routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :words
+  resources :words, only: [:index]
   # Defines the root path route ("/")
   root to: "words#index"
   # root "articles#index"
+  # TODO change the root, so home page has api key option and profile management
+  resources :api_keys, only: [:create, :destroy, :index]
 end
